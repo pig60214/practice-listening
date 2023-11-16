b52a44ee82905ae21bd40ca1d637ca9590a2c4a2
YX <pig60214@gmail.com>
2023-11-15 23:03:40 -0500

    Parse DB Result To Interface

---
+++ src/interfaces/transcription.ts
@@ -0,0 +1,7 @@
+interface ITranscription {
+  id: number,
+  title: string,
+  content: string,
+  youtubeUrl: string,
+  createDate: string,
+}
--- src/repositories/transcription.ts
+++ src/repositories/transcription.ts
@@ -1,9 +1,11 @@
 import pool from './connect';
+import { objectToCamel } from 'ts-case-convert';
 
-async function getTranscriptions() {
+async function getTranscriptions(): Promise<ITranscription[]>{
   try {
     const result = await pool.query(`SELECT * FROM transcription`);
-    return result.rows[0];
+    const data = objectToCamel(result.rows) as ITranscription[];
+    return data;
   } catch (err) {
     console.error('SQL error', err);
     return [];
