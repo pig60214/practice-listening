5565f11b41a3e531f2c6f2eadef4741a3a15ea3a
YX <pig60214@gmail.com>
2023-11-26 17:55:37 -0500

    Adjust DB Schema

--- db_schema/transcription.sql
+++ db_schema/transcription.sql
@@ -2,6 +2,6 @@ CREATE TABLE transcription (
    id SERIAL PRIMARY KEY,
    title VARCHAR(100) NOT NULL,
    content VARCHAR NOT NULL,
-   youtube_url VARCHAR(50) NOT NULL,
+   youtube_url VARCHAR(100) NOT NULL,
    create_date date NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
