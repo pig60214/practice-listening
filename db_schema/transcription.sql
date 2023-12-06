f2ed3d5bdd4865913981debbb1708779ebb6d982
YX <pig60214@gmail.com>
2023-12-05 21:05:41 -0500

    Seperate Test Data

--- db_schema/transcription.sql
+++ db_schema/transcription.sql
@@ -3,5 +3,6 @@ CREATE TABLE transcription (
    title VARCHAR(100) NOT NULL,
    content VARCHAR NOT NULL,
    youtube_url VARCHAR(100) NOT NULL,
-   create_date date NOT NULL DEFAULT CURRENT_TIMESTAMP
+   create_date date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   is_test INT NOT NULL DEFAULT(0)
 );
